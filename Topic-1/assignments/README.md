cf2724a03b8b7739ac986897c1e245f4b3fba2b7
Junjie <46977478+JunjieLeiCoe@users.noreply.github.com>
2021-08-26 02:39:44 -0700

    add coin-flip()

--- Topic-1/assignments/README.md
+++ Topic-1/assignments/README.md
@@ -1,17 +1,1 @@
-# Print statement in Python
-In the `divmod` function that predefined in Python3, it will return 2 values. 
-Extarct these 2 values and print them in a formatted manner. 
 
-The expect output should be like this:
-
-```text
-The divmod output for 177 and 7 is: 
------------------------------------
-17
-7
-```
-
-# Git & Github;
-Commit that python scripts to your github online repositoty under assignment-1
-
-#  
---
+++ Topic-1/coin-flip.py
@@ -0,0 +1,33 @@
+import random
+
+
+def coin():
+    """simulate a coin flip()
+
+    Returns:
+        str: ["heads" or "tails"]
+    """
+
+
+def coin_10flips_1():
+    """Simulate 10 coin flips
+
+    Returns:
+        [list or string]: [lst should contains heads or tails]
+    """
+
+
+def coin_10flips_2():
+    """[The second way to flip a coin for 10 times]
+    """
+
+
+def count_head(n):
+    """count how many times the coin is HEAD and how many times the coin is TAIL
+
+    Args:
+        n (int): Number of times the coin is going to flipped
+
+    Returns:
+        Tuple : (heads, tails)
+    """
